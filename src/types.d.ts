fc371daf352ccf2645d3e8f31a96cbab25c6876d
Edward <edward@chatbotboy.ai>
2026-02-21 21:09:55 -0600

    Update iconify-icon typescript declaration

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,16 +1,13 @@
-import * as React from 'react';
+import React from 'react';
 
 declare global {
     namespace JSX {
         interface IntrinsicElements {
-            'iconify-icon': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
-                icon?: string;
-                width?: string | number;
-                height?: string | number;
-                inline?: boolean;
-                flip?: string;
-                rotate?: string | number;
-            };
+            'iconify-icon': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & {
+                icon: string;
+                width?: string;
+                height?: string;
+            }, HTMLElement>;
         }
     }
 }
